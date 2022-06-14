799804c6486ba664f5ab69ee605e48a8dcffb7fd
Ewe Barbo <ewebarbo@gmail.com>
2022-06-14 18:06:18 -0300

    adicionado var isGerente para path /home.

--- src/main/java/br/ufsm/csi/integrador/expomanager/controller/IndexController.java
+++ src/main/java/br/ufsm/csi/integrador/expomanager/controller/IndexController.java
@@ -1,13 +1,15 @@
 package br.ufsm.csi.integrador.expomanager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/", "/home"})
-    public String teste() {
+    public String getIndex(Model model) {
+        model.addAttribute("isGerente", true);
         return "home";
     }
 }
