b364ba71563e8cfee6c1de5b34f3fd338d41604a
Ewe Barbo <ewebarbo@gmail.com>
2022-06-19 15:45:39 -0300

    iniciado a inserção de um novo colaborador. WORK IN PROGRESS.

--- src/main/java/br/ufsm/csi/integrador/expomanager/controller/ColaboradorController.java
+++ src/main/java/br/ufsm/csi/integrador/expomanager/controller/ColaboradorController.java
@@ -1,15 +1,37 @@
 package br.ufsm.csi.integrador.expomanager.controller;
 
+import br.ufsm.csi.integrador.expomanager.model.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class ColaboradorController {
 
-    @GetMapping({"/novo-colaborador.action", "/novo-colaborador"})
-    public String getNovoColaborador(Model model) {
+    @GetMapping({"/cadastro-colaborador.action", "/cadastro-colaborador"})
+    public String getCadastroColaborador(Model model) {
         model.addAttribute("isGerente", true);
-        return "novo-colaborador";
+        return "cadastro-colaborador";
+    }
+
+    @GetMapping({"/colaboradores.action", "/colaboradores"})
+    public String getColaboradores(Model model) {
+        model.addAttribute("isGerente", true);
+        return "colaboradores";
+    }
+
+    @GetMapping(value = {"/salvarColaborador.action", "/salvarColaborador"})
+    public String saveColaborador(Model model, HttpServletRequest request) {
+        String nome = request.getParameter("nomeColaborador");
+        String email = request.getParameter("emailColaborador");
+        String senha = request.getParameter("senhaColaborador");
+
+        Usuario colaborador = new Usuario(nome, email, senha, false, true);
+
+        model.addAttribute("isGerente", true);
+        model.addAttribute("colaborador", colaborador);
+        return "colaboradores";
     }
 }
