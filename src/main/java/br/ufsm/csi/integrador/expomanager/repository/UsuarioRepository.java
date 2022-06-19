6c31f9d73ab040c756dd039d0182d80617bd81a2
Ewe Barbo <ewebarbo@gmail.com>
2022-06-19 20:33:30 -0300

    usuario repository inicializado, não finalizado.

---
+++ src/main/java/br/ufsm/csi/integrador/expomanager/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package br.ufsm.csi.integrador.expomanager.repository;
+
+import br.ufsm.csi.integrador.expomanager.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    @Override
+    List<Usuario> findAll();
+
+    @Query(value = "SELECT * FROM USUARIOS US WHERE US.NOME = ?", nativeQuery = true)
+    Usuario findUsuarioByNome(@Param("nome") String nome);
+}
