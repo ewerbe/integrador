fa2ff7e1e36e4bb289429e20d41fb796b8119aef
Ewe Barbo <ewebarbo@gmail.com>
2022-06-26 15:44:00 -0300

    retirado comentários.

--- src/main/java/br/ufsm/csi/integrador/expomanager/repository/UsuarioRepository.java
+++ src/main/java/br/ufsm/csi/integrador/expomanager/repository/UsuarioRepository.java
@@ -2,17 +2,6 @@ package br.ufsm.csi.integrador.expomanager.repository;
 
 import br.ufsm.csi.integrador.expomanager.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    //TODO: acho que não vou usar.
-//    @Override
-//    List<Usuario> findAll();
-//
-//    @Query(value = "SELECT * FROM USUARIOS US WHERE US.NOME = ?", nativeQuery = true)
-//    Usuario findUsuarioByNome(@Param("nome") String nome);
 }
